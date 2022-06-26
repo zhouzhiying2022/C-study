30e1f3f694aecc491eec18fd407f791080cda375
智颖 周 <sfznaniaiwoma@163.com>
2022-06-26 22:52:08 +0800

    第五天

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+const unsigned char allChar[63] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+void generateString(unsigned char* dest, unsigned int len);
+
 int main()
 {
 	using namespace std;
@@ -43,5 +46,30 @@ int main()
 	//明确指出浮点常量的类型 float F或f long double L或l
 
 
+	const unsigned int strlen = 20;
+
+	unsigned char str[strlen + 1] = {};
+
+	generateString(str, 2);
+
+	cout << str << endl;
+
 	return 0;
+}
+
+
+void generateString(unsigned char* dest, const unsigned int len)
+{
+	unsigned int cnt, randNo;
+
+	srand((unsigned int)time(NULL));
+
+	for (cnt = 0; cnt < len; cnt++)
+	{
+		randNo = rand() % 62;
+		*dest = allChar[randNo];
+		dest++;
+	}
+
+	*dest = '\0';
 }
--- day5/day5.cpp
+++ day5/day5.cpp
@@ -4,6 +4,54 @@ int main()
 {
 	using namespace std;
 
+	//算术运算符 结合性:左结合 先结算左边的运算符,再计算右边的运算符
+
+	int a = ( 3 + 5) * 7/2 + 5;
+	int b = 3 + 5  * 7 - 2;
+	cout << a << endl;
+	cout << b << endl;
+
+	//类型转换
+	//赋值或初始化时,如果右边的表达式计算结果类型与左边的变量类型不同:右边值被转换成左边变量类型
+	//函数参数传递时:实际参数被转换成形式参数类型 
+	//表达式的运算数类型不一致,有一套转换规则 float double longdouble 
+
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+
+  //ios_base:floatfield	ios_base::fixed	使用定点计数法，如1234.5
+   //ios_base::scientific	使用科学计数法，如1.2345e4
+
+
+	float ff = 3;
+	int ii = 3.9832;
+	short ss = 10;
+	double dd = ss;
+	cout << ff << endl << ii << endl << ss << endl << dd << endl;
+
+
+	 
+
+
+
+
+
+
+
+
+
+
+
+	 
+	
+
+
+
+
+
+
+
+
+
 
 
 
